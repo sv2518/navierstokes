eef31df367f438ef51c02862100c00d1a1e29fcb
Sophia Vorderwuelbecke <sv2518@dyn47-98.me.ic.ac.uk>
2019-06-05 11:40:07 +0100

    latest

--- SaddlePointNaive.py
+++ SaddlePointNaive.py
@@ -8,10 +8,11 @@ def both(expr):
 
 def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
     #generate mesh
-    LX=1.0
+    LX=2.0
     LY=2.0
     mesh = RectangleMesh(2 ** mesh_size, 2 ** mesh_size,Lx=LX,Ly=LY,quadrilateral=True)
-    
+    mesh.coordinates.dat.data[:,0]-=0.5
+
     #function spaces
     U = FunctionSpace(mesh, "RTCF",1)
     P = FunctionSpace(mesh, "DG", 0)
@@ -24,12 +25,16 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
 	
     #building the operators
     n=FacetNormal(W.mesh())
-    nue=Constant(1)#viscosity
+    nue=Constant(0.4)#re=40
 
     #specify inflow/initial solution
     x,y=SpatialCoordinate(mesh)
     lam=(-8.*pi**2/(nue**(-1)+sqrt(nue**(-2)+16*pi**2)))
-    inflow=Function(U).project(as_vector((1-exp(lam*x)*cos(2*pi*y),0.0)))
+    ux=1-exp(lam*x)*cos(2*pi*y)
+    uy=lam/(2*pi)*exp(lam*x)*sin(2*pi*y)
+    p_sol=Function(P).project(-1./2*exp(2*lam*x))
+    u_sol=Function(U).project(as_vector((ux,uy)))
+    inflow=Function(U).project(as_vector((ux,0.0)))
     #inflow_uniform=Function(U).project(Constant((1.0,0.0)))  
     
     #Picard iteration
@@ -40,9 +45,10 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
         #Laplacian
         alpha=Constant(10.)
         gamma=Constant(10.) 
-        kappa1=nue * alpha*Constant(mesh_size)#??????
-        kappa2=nue * gamma*Constant(mesh_size)#???????
-      #  g=Function(U).project(as_vector((0.0,lam/(2*pi)*exp(lam*x)*sin(2*pi*y))))
+        kappa1=nue * alpha/Constant(LY/2**mesh_size)#??????
+        kappa2=nue * gamma/Constant(LY/2**mesh_size)#???????
+
+        #g=Function(U).project(as_vector((tang,0.0))) #tangential comp
         g=Constant((0.0,0.0))
         a_dg=(nue*inner(grad(u),grad(v))*dx
             -inner(outer(v,n),nue*grad(u))*ds 
@@ -74,6 +80,8 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
 
         #boundary conditions
         bc_1=[]
+        bc0=DirichletBC(W.sub(1),p_sol,1)
+        bc_1.append(bc0)
         bc1=DirichletBC(W.sub(0),inflow,1)#plane x=0
         bc_1.append(bc1)
         bc2=DirichletBC(W.sub(0),Constant((0.0,0.0)),3)#plane y=0
@@ -81,10 +89,13 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
         bc3=DirichletBC(W.sub(0),Constant((0.0,0.0)),4)#plane y=L
         bc_1.append(bc3)
 
+        #add nullspace
+        nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
+
         #build problem and solver
         w = Function(W)
         problem = LinearVariationalProblem(a, L, w, bc_1)
-        solver = LinearVariationalSolver(problem, solver_parameters=parameters)
+        solver = LinearVariationalSolver(problem, nullspace=nullspace,solver_parameters=parameters)
         solver.solve()
         u1,p1=w.split()
 
@@ -92,24 +103,23 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
         eps=errornorm(u1,u_linear)#l2 by default
         counter+=1
         print("Picard iteration change in approximation",eps,", counter: ",counter)
-        if(eps<10**(-8)):
+        if(eps<10**(-12)):
             print("Picard iteration converged")  
             break          
         else:
             u_linear.assign(u1)
 
-        #method of manufactured solutions
-        test=Function(W)
-        p_sol=Function(P).project(-1./2*exp(2*lam*x))
-        u_sol=Function(U).project(as_vector((1-exp(lam*x)*cos(2*pi*y),lam/(2*pi)*exp(lam*x)*sin(2*pi*y))))
-        test.sub(0).assign(u_sol)
-        test.sub(1).assign(p_sol)#why does it not matter if I take this in or not?
-        #plt.plot((assemble(action(a-L,test),bcs=bc_1).dat.data[0]))#maxnorm
-        #plt.show()
+    #method of manufactured solutions
+    test=Function(W)
+    test.sub(0).assign(u_sol)
+    test.sub(1).assign(p_sol)
+    plt.plot((assemble(action(a-L,test),bcs=bc_1).dat.data[0]))
+    plt.show()
 
-        print("velo error",errornorm(u1,u_sol,norm_type="Hdiv"),", pres error: ",errornorm(p1,p_sol))
-        
-
+    print("velo error",errornorm(u_sol,u1,norm_type="Hdiv"),", pres error: ",errornorm(p1,p_sol,norm_type="L2"))
+    divtest=Function(P)
+    divtest.assign(project(div(u1),P))
+    print("div error",errornorm(u_sol,Function(U),norm_type="L2"))
     return w
 
 
@@ -125,7 +135,7 @@ parameters={
 print("Channel Flow")
 print("Cell number","IterationNumber")
 
-for n in range(2,8):#increasing element number
+for n in range(4,8):#increasing element number
     
     #solve
     w = solve_problem(n, parameters,aP=None, block_matrix=False)
