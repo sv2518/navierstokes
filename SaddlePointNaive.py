12003140ed530d12b12b93a9254f394b20c393e5
Colin Cotter <colin.cotter@imperial.ac.uk>
2019-06-18 13:33:01 +0100

    fix the forms and move the solvers outside the loop

--- SaddlePointNaive.py
+++ SaddlePointNaive.py
@@ -70,87 +70,85 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
 
     #Picard iteration
     u_linear=Function(U).assign(inflow_uniform)
+
+    #Laplacian
+    alpha=Constant(10)#interior
+    gamma=Constant(10) #exterior
+    h=CellVolume(mesh)/FacetArea(mesh)  
+    havg=avg(CellVolume(mesh))/FacetArea(mesh)
+    kappa1=nue*alpha/havg
+    kappa2=nue*gamma/h
+    
+    x, y = SpatialCoordinate(mesh)
+    g_neg=Function(U).project(Constant((0.0,0.0)))
+    g_pos=Function(U).project(as_vector([-x*100*(x-1)/25*U_inf,0]))
+    #g=Constant((0.0,0.0))
+    
+    #a_dg for other sides
+    a_dg=(nue*inner(grad(u),grad(v))*dx
+          -inner(outer(v,n),nue*grad(u))*ds((1,2,3)) 
+          -inner(outer(u,n),nue*grad(v))*ds((1,2,3))
+          +kappa2*inner(v,u)*ds((1,2,3))
+          -inner(nue*avg(grad(v)),both(outer(u,n)))*dS
+          -inner(both(outer(v,n)),nue*avg(grad(u)))*dS
+          +kappa1*inner(both(outer(u,n)),both(outer(v,n)))*dS)
+    
+    #a_dg for lid
+    a_dg+=(
+        -inner(outer(v,n),nue*grad(u-g_pos))*ds((4))
+        -inner(outer(u-g_pos,n),nue*grad(v))*ds((4)) 
+        +kappa2*inner(v,u-g_pos)*ds((4))
+    )
+    
+    #Advection
+    #for lid
+    un = 0.5*(dot(u_linear, n)+ abs(dot(u_linear, n)))
+    
+    adv_dg=(dot(u_linear,div(outer(v,u)))*dx#like paper
+            -dot(v,u*un)*ds(4)#similar to matt piggots
+            -dot((v('+')-v('-')),(un('+')*(u('+')) - un('-')*(u('-'))))*dS)#like in the tutorial
+    
+    #for other thingis
+    #un_neg = 0.5*(dot(u_linear, n)+sign(dot(u_linear, n))*dot(g_neg,n)+abs(dot(g_neg,n))+ abs(dot(u_linear, n)))
+    adv_dg+=-inner(v,u*un)*ds((1,2,3)) #similar to matt piggots
+    
+    #form
+    eq = a_dg+Constant(-1.)*adv_dg-div(v)*p*dx+div(u)*q*dx
+    #f=dot(Function(U),v)*dx   
+    #eq +=f
+    a=lhs(eq)
+    L=rhs(eq)
+    
+    #preconditioning(not used here)
+    if aP is not None:
+        aP = aP(W)
+    if block_matrix:
+        mat_type = 'nest'
+    else:
+        mat_type = 'aij'
+            
+    #boundary conditions
+    bc_1=[]
+    bc0=DirichletBC(W.sub(0),Constant((0.0,0.0)),1)
+    bc_1.append(bc0)
+    bc1=DirichletBC(W.sub(0),Constant((0.0,0.0)),2)#plane x=0
+    bc_1.append(bc1)
+    bc2=DirichletBC(W.sub(0),Constant((0.0,0.0)),3)#plane y=0
+    bc_1.append(bc2)
+    bc3=DirichletBC(W.sub(0),Constant((0.0,.0)),4)#plane y=L
+    bc_1.append(bc3)
+    
+    #add nullspace bc all boundaries are specified
+    nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
+
+    #build problem and solver
+    w = Function(W)
+    problem = LinearVariationalProblem(a, L, w, bc_1)
+    solver = LinearVariationalSolver(problem,nullspace=nullspace,solver_parameters=parameters)
+
     counter=0
     while(True):
 
-        #Laplacian
-        alpha=Constant(10)#interior
-        gamma=Constant(10) #exterior
-        h=CellVolume(mesh)/FacetArea(mesh)  
-        havg=avg(CellVolume(mesh))/FacetArea(mesh)
-        kappa1=nue*alpha/havg
-        kappa2=nue*gamma/h
-
-        x, y = SpatialCoordinate(mesh)
-        g_neg=Function(U).project(Constant((0.0,0.0)))
-        g_pos=Function(U).project(as_vector([-x*100*(x-1)/25*U_inf,0]))
-        #g=Constant((0.0,0.0))
-
-        #a_dg for other sides
-        a_dg=(nue*inner(grad(u),grad(v))*dx
-            -inner(outer(v,n),nue*grad(u))*ds((1,2,3)) 
-            -inner(outer(u-g_neg,n),nue*grad(v))*ds((1,2,3)) 
-            +kappa2*inner(v,u-g_neg)*ds((1,2,3))
-            -inner(nue*avg(grad(v)),both(outer(u-g_neg,n)))*dS((1,2,3))
-            -inner(both(outer(v,n)),nue*avg(grad(u)))*dS((1,2,3))
-            +kappa1*inner(both(outer(u-g_neg,n)),both(outer(v,n)))*dS((1,2,3)))
-
-        #a_dg for lid
-        a_dg+=(-inner(outer(v,n),nue*grad(g_pos))*ds((4))
-            -inner(outer(g_pos,n),nue*grad(v))*ds((4)) 
-            +kappa2*inner(v,g_pos)*ds((4))
-            -inner(both(outer(v,n)),nue*avg(grad(u-g_pos)))*dS((4))
-            -inner(nue*avg(grad(v)),both(outer(u-g_pos,n)))*dS((4))
-            +kappa1*inner(both(outer(u-g_pos,n)),both(outer(v,n)))*dS((4)))
-         
-        #Advection
-        #for lid
-        un = 0.5*(dot(u_linear, n)+ abs(dot(u_linear, n)))
-        
-        un_pos = 0.5*(dot(u_linear, n)+sign(dot(u_linear,n))*(dot(g_pos,n))+abs(dot(g_pos,n))+ abs(dot(u_linear, n)))
-        adv_dg=(dot(u_linear,div(outer(v,u)))*dx#like paper
-            -dot(v,u*un_pos)*ds(4)#similar to matt piggots
-            -dot((v('+')-v('-')),(un_pos('+')*(u('+')) - un_pos('-')*(u('-'))))*dS(4))#like in the tutorial
-    
-        #for other thingis
-        #un_neg = 0.5*(dot(u_linear, n)+sign(dot(u_linear, n))*dot(g_neg,n)+abs(dot(g_neg,n))+ abs(dot(u_linear, n)))
-        adv_dg+=(-inner(v,u*un)*ds((1,2,3)) #similar to matt piggots
-            -dot((v('+')-v('-')),(un('+')*(u('+')) - un('-')*(u('-'))))*dS((1,2,3)) )#like in the tutorial
-    
-
-        #form
-        eq = a_dg+Constant(-1.)*adv_dg-div(v)*p*dx+div(u)*q*dx
-        f=dot(Function(U),v)*dx   
-        eq +=f
-        a=lhs(eq)
-        L=rhs(eq)
-
-        #preconditioning(not used here)
-        if aP is not None:
-            aP = aP(W)
-        if block_matrix:
-            mat_type = 'nest'
-        else:
-            mat_type = 'aij'
-
-        #boundary conditions
-        bc_1=[]
-        bc0=DirichletBC(W.sub(0),Constant((0.0,0.0)),1)
-        bc_1.append(bc0)
-        bc1=DirichletBC(W.sub(0),Constant((0.0,0.0)),2)#plane x=0
-        bc_1.append(bc1)
-        bc2=DirichletBC(W.sub(0),Constant((0.0,0.0)),3)#plane y=0
-        bc_1.append(bc2)
-        bc3=DirichletBC(W.sub(0),Constant((0.0,.0)),4)#plane y=L
-        bc_1.append(bc3)
-
-        #add nullspace bc all boundaries are specified
-        nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
-
-        #build problem and solver
-        w = Function(W)
-        problem = LinearVariationalProblem(a, L, w, bc_1)
-        solver = LinearVariationalSolver(problem,nullspace=nullspace,solver_parameters=parameters)
         solver.solve()
         u1,p1=w.split()
 
@@ -163,6 +161,8 @@ def solve_problem(mesh_size, parameters, aP=None, block_matrix=False):
             break          
         else:
             u_linear.project(u1)
+            u1.assign(0.)
+            p1.assign(0.)
             #one comp has to be set to 0?
 
 
@@ -224,31 +224,30 @@ for n in refin:#increasing element number
 
     
     #plot solutions
-    File("poisson_mixed_velocity_.pvd").write(u)
-    File("poisson_mixed_pressure_.pvd").write(p)
+    File("cavity.pvd").write(u,p)
     try:
         import matplotlib.pyplot as plt
     except:
         warning("Matplotlib not imported")
 
     #plot solutions
-    try:
-        plot_velo_pres(u,p)
-    except:
-        warning("Cannot show figure")
+    #try:
+    #    plot_velo_pres(u,p)
+    #except:
+    #    warning("Cannot show figure")
 
 #plot convergence
-plot_convergence_velo_pres(error_velo,error_pres,list_N)
+#plot_convergence_velo_pres(error_velo,error_pres,list_N)
 
 #plot actual errors in L2
-plt.plot(refin,np.array([3.9,1.2,0.33,0.093,0.026]),label="paper")
-plt.plot(refin,np.array(error_pres)/100,label="mine/100")
-plt.legend()
-plt.title("L2 error pressure")
-plt.show()
+#plt.plot(refin,np.array([3.9,1.2,0.33,0.093,0.026]),label="paper")
+#plt.plot(refin,np.array(error_pres)/100,label="mine/100")
+#plt.legend()
+#plt.title("L2 error pressure")
+#plt.show()
 
-plt.plot(refin,np.array([0.23,0.062,0.016,0.0042,0.0011]),label="paper")
-plt.plot(refin,error_velo,label="mine")
-plt.legend()
-plt.title("L2 error velocity")
-plt.show()
+#plt.plot(refin,np.array([0.23,0.062,0.016,0.0042,0.0011]),label="paper")
+#plt.plot(refin,error_velo,label="mine")
+#plt.legend()
+#plt.title("L2 error velocity")
+#plt.show()
