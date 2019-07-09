1c44e85d499640c14627bc10a1227f56ba1dce1a
Sophia Vorderwuelbecke <sv2518@dyn47-217.me.ic.ac.uk>
2019-07-09 10:18:13 +0100

    more structural changes

--- forms/spcsforms.py
+++ forms/spcsforms.py
@@ -1,8 +1,12 @@
 from firedrake import *
-from forms.Operators import *
+from forms.operators import *
 
-def buildPredictorForm(p_init_sol,u_init_sol,nue,mesh,U,P,W,U_inf,dt,bc_tang,v_k,u_n,p_n):
+def build_predictor_form(W,dt,mesh,nue,bc_tang,v_k,u_n,p_n):
     print("....build predictor")
+
+    #subspaces
+    U=W.sub(0)
+    P=W.sub(1)
 
     #functions
     v_knew=TrialFunction(U)
@@ -13,33 +17,42 @@ def buildPredictorForm(p_init_sol,u_init_sol,nue,mesh,U,P,W,U_inf,dt,bc_tang,v_k
     ubar_k=Constant(0.5)*(u_n+v_k) #init old midstep
     ubar_knew=Constant(0.5)*(u_n+v_knew) #init new midstep  
     
-    lapl_dg=DiffusionOperator(nue,ubar_knew,v,n,bc_tang,mesh)
-    adv_dg=AdvectionOperator(ubar_k,ubar_knew,v,n,bc_tang)
-    pres_dg=Product(div(v),p_n)
-   
-    #Time derivative
+    lapl_dg=diffusion_operator(nue,ubar_knew,v,n,bc_tang,mesh)
+    adv_dg=advection_operator(ubar_k,ubar_knew,v,n,bc_tang)
+    pres_dg=ibp_product(div(v),p_n)
     time=1/Constant(dt)*inner(v_knew-u_n,v)*dx
 
     eq_pred=time+adv_dg-lapl_dg+pres_dg
 
     return eq_pred
 
-def buildPressureForm(W,U,P,dt,mesh,U_inf,bc_tang,div_old):
+def build_update_form(W,dt,mesh,bc_tang,div_old):
     print("....build pressure update")
 
+    #subspaces
+    U=W.sub(0)
+    P=W.sub(1)
+
+    #functions
     w,beta = TrialFunctions(W)
     v,q = TestFunctions(W)
 
-    force_dg_pres=Product(div_old/dt ,q)
-    incomp_dg_pres=Product(div(w) ,q)
-    pres_dg_pres=Product(div(v),beta)
+    force_dg_upd=ibp_product(div_old/dt ,q)
+    incomp_dg_upd=ibp_product(div(w) ,q)
+    pres_dg_upd=ibp_product(div(v),beta)
     
-    eq_pres=dot(w,v)*dx+force_dg_pres-incomp_dg_pres-pres_dg_pres 
+    eq_upd=dot(w,v)*dx+force_dg_upd-incomp_dg_upd-pres_dg_upd
 
-    return eq_pres
+    return eq_upd
 
-def buildCorrectorForm(W,U,P,dt,mesh,U_inf,v_knew_hat,beta):
+def build_corrector_form(W,dt,mesh,v_knew_hat,beta):
     print("....build corrector")
+    
+    #subspaces
+    U=W.sub(0)
+    P=W.sub(1)
+
+    #functions
     v_knew=TrialFunction(U)
     v=TestFunction(U)
 
