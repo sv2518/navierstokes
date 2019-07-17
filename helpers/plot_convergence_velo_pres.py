bd6251882e0819c3747f020b8a9939f00ebd1cb9
Sophia Vorderwuelbecke <sv2518@dyn47-98.me.ic.ac.uk>
2019-07-17 11:24:53 +0100

    convergence tests

--- helpers/plot_convergence_velo_pres.py
+++ helpers/plot_convergence_velo_pres.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_convergence_velo_pres(error_velo,error_pres,list_N):
+def plot_convergence_velo_pres(error_velo,error_pres,list_N,value):
     # velocity convergence plot
     fig = plt.figure()
     axis = fig.gca()
-    linear=error_velo[::-1]
-    axis.loglog(list_N,linear,label='$||e_u||_{\infty}$')
-    axis.loglog(list_N,0.0001*np.power(list_N,2),'r*',label="second order")
-    axis.loglog(list_N,0.0001*np.power(list_N,1),'g*',label="first order")
+    linear=error_velo
+    axis.semilogy(list_N,linear,label='$||e_u||_{\infty}$')
+    axis.semilogy(list_N,value*np.power(list_N,2),'r*',label="second order")
+    axis.semilogy(list_N,value*np.power(list_N,1),'g*',label="first order")
     axis.set_xlabel('$2**Level$')
     axis.set_ylabel('$Error$')
     axis.legend()
@@ -19,10 +19,10 @@ def plot_convergence_velo_pres(error_velo,error_pres,list_N):
     #pressure convergence plot
     fig = plt.figure()
     axis = fig.gca()
-    linear=error_pres[::-1]
+    linear=error_pres
     axis.loglog(list_N,linear,label='$||e_p||_{\infty}$')
-    axis.loglog(list_N,0.1*np.power(list_N,2),'r*',label="second order")
-    axis.loglog(list_N,0.1*np.power(list_N,1),'g*',label="first order")
+    axis.loglog(list_N,1*value*np.power(list_N,2),'r*',label="second order")
+    axis.loglog(list_N,1*value*np.power(list_N,1),'g*',label="first order")
     axis.set_xlabel('$2**Level$')
     axis.set_ylabel('$Error$')
     axis.legend()
