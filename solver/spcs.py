b19c6866f5e559a2a740f55a35b5e255231542ca
Sophia Vorderwuelbecke <sv2518@dyn47-217.me.ic.ac.uk>
2019-07-26 13:37:21 +0100

    profiling

--- solver/spcs.py
+++ solver/spcs.py
@@ -3,168 +3,169 @@ from forms.spcsforms import *
 from solver.initialvalues import *
 from solver.parameters import *
 import matplotlib.pyplot as plt
+from firedrake.petsc import PETSc
 
 #standard pressure correction scheme
 def spcs(W,mesh,nue,bc,U_inf,t,dt,T,outfile,u_init=None,p_init=None,output=False):
 
-    #functions and normal
-    U=W.sub(0)
-    P=W.sub(1)
-    u,p = TrialFunctions(W)
-    v,q = TestFunctions(W)
-    f =Function(U)
+    with PETSc.Log.Event("spcs configuration"):
+        #functions and normal
+        U=W.sub(0)
+        P=W.sub(1)
+        u,p = TrialFunctions(W)
+        v,q = TestFunctions(W)
+        f =Function(U)
 
-    #get solver parameters
-    [parameters_corr,parameters_pres,parameters_pres_better,parameters_velo,parameters_velo_initial,nn]=defineSolverParameters()
+        #get solver parameters
+        [parameters_corr,parameters_pres,parameters_pres_better,parameters_velo,parameters_velo_initial,nn]=defineSolverParameters()
 
-    #split up boundary conditions
-    [bc_norm,bc_tang,bc_expr]=bc
+        #split up boundary conditions
+        [bc_norm,bc_tang,bc_expr]=bc
 
-    print("\nCALCULATE INITIAL VALUES")########################################################
-    #check if analytical initial condition is given
-    if(u_init):
-        u_init_sol=Function(U).project(u_init)
-    else:
-        #calculate inital velocity with potential flow
-        u_init_sol=initial_velocity(W,dt,mesh,bc,nue)
+    with PETSc.Log.Event("initial values"):
+        print("\nCALCULATE INITIAL VALUES")########################################################
+        #check if analytical initial condition is given
+        if(u_init):
+            u_init_sol=Function(U).project(u_init)
+        else:
+            #calculate inital velocity with potential flow
+            u_init_sol=initial_velocity(W,dt,mesh,bc,nue)
 
+        
+        divtest=Function(P).project(div(u_init_sol))
+        print("Div error of initial velocity",errornorm(divtest,Function(P)))
+
+        #check in analytical solutions is given
+        #if p_init:
+            #x,y=SpatialCoordinate(W.mesh())
+            #p_init_sol=Function(P).project(p_init)
+        #else:
+            #with that initial value calculate intial pressure 
+            # with Poission euqation including some non-divergence free velocity
+        p_init_sol=initial_pressure(W,dt,mesh,nue,bc,u_init_sol)
     
-    divtest=Function(P).project(div(u_init_sol))
-    print("Div error of initial velocity",errornorm(divtest,Function(P)))
-    plt.show()
-
-    #check in analytical solutions is given
-   # if p_init:
-   #     x,y=SpatialCoordinate(W.mesh())
-   #     p_init_sol=Function(P).project(p_init)
-   # else:
-        #with that initial value calculate intial pressure 
-        # with Poission euqation including some non-divergence free velocity
-    p_init_sol=initial_pressure(W,dt,mesh,nue,bc,u_init_sol)
-    
-
-    print("\nBUILD FORMS")#####################################################################
-    v_k=Function(U)
-    u_n=Function(U)
-    p_n=Function(P)   
-    div_old=Function(P)
-    v_knew_hat=Function(U)
-    beta=Function(P)
-    eq_pred=build_predictor_form(W,dt,mesh,nue,bc_tang,v_k,u_n,p_n)
-    eq_upd=build_update_form(W,dt,mesh,bc_tang,div_old)
-    eq_corr=build_corrector_form(W,dt,mesh,v_knew_hat,beta)
+    with PETSc.Log.Event("build forms"):
+        print("\nBUILD FORMS")#####################################################################
+        v_k=Function(U)
+        u_n=Function(U)
+        p_n=Function(P)   
+        div_old=Function(P)
+        v_knew_hat=Function(U)
+        beta=Function(P)
+        eq_pred=build_predictor_form(W,dt,mesh,nue,bc_tang,v_k,u_n,p_n)
+        eq_upd=build_update_form(W,dt,mesh,bc_tang,div_old)
+        eq_corr=build_corrector_form(W,dt,mesh,v_knew_hat,beta)
 
     #initialise time stepping
     u_n.assign(u_init_sol)
     p_n.assign(p_init_sol)
 
     #save & divtest
-    outfile.write(u_n,p_n,time=0)
+    if output:
+        outfile.write(u_n,p_n,time=0)
+
     divtest=Function(P).project(div(u_n))
     print("Div error of initial velocity",errornorm(divtest,Function(P)))
 
-
-    print("\nBUILD PROBLEM AND SOLVERS")########################################################
-    #predictor
-    nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
-    def nullspace_basis(T):
-        return VectorSpaceBasis(constant=True)
-    appctx = {'trace_nullspace': nullspace_basis}
-    w_pred = Function(U)
-    predictor = LinearVariationalProblem(lhs(eq_pred),rhs(eq_pred), w_pred,bc_norm)
-    solver_pred = LinearVariationalSolver(predictor, solver_parameters=parameters_velo)
-
-    #update
-    w_upd = Function(W)
-    nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
-    update= LinearVariationalProblem(lhs(eq_upd),rhs(eq_upd),w_upd,bc_norm)#BC RIGHT???
-    solver_upd= LinearVariationalSolver(update,solver_parameters=parameters_pres,appctx=appctx)
+    with PETSc.Log.Event("build problems and solvers"):
+        print("\nBUILD PROBLEM AND SOLVERS")########################################################
         
-    #corrector
-    w_corr = Function(U)
-    corrector= LinearVariationalProblem(lhs(eq_corr),rhs(eq_corr), w_corr,bc_norm)
-    solver_corr = LinearVariationalSolver(corrector,solver_parameters=parameters_corr)
+        nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
+        def nullspace_basis(T):
+            return VectorSpaceBasis(constant=True)
+        appctx = {'trace_nullspace': nullspace_basis}
 
-    print("\nTIME PROGRESSING")################################################################
-    #outerloop for time progress
-    n = 1
-    while n < (T+1) :
-        #update time-dependent boundary
-        t.assign(n)
-        print("t is: ",n*dt)
-        print("n is: ",n)
+        with PETSc.Log.Event("predictor"):
+            w_pred = Function(U)
+            predictor = LinearVariationalProblem(lhs(eq_pred),rhs(eq_pred), w_pred,bc_norm)
+            solver_pred = LinearVariationalSolver(predictor, solver_parameters=parameters_velo)
 
-        if bc_tang:
-            bc_tang[0]=[bc_tang[0][0].project(bc_expr),4]
-            bc_tang[1]=[bc_tang[1][0].project(bc_expr),1]
-            bc_tang[2]=[bc_tang[2][0].project(bc_expr),2]
-            bc_tang[3]=[bc_tang[3][0].project(bc_expr),3]
+        with PETSc.Log.Event("update"):
+            w_upd = Function(W)
+            nullspace=MixedVectorSpaceBasis(W,[W.sub(0),VectorSpaceBasis(constant=True)])
+            update= LinearVariationalProblem(lhs(eq_upd),rhs(eq_upd),w_upd,bc_norm)#BC RIGHT???
+            solver_upd= LinearVariationalSolver(update,solver_parameters=parameters_pres,appctx=appctx)
+            
+        with PETSc.Log.Event("corrector"):
+            w_corr = Function(U)
+            corrector= LinearVariationalProblem(lhs(eq_corr),rhs(eq_corr), w_corr,bc_norm)
+            solver_corr = LinearVariationalSolver(corrector,solver_parameters=parameters_corr)
 
-      
-        #update start value of picard iteration       
-        counter=0
-        v_k.assign(u_n)
+    with PETSc.Log.Event("time progressing"):
+        print("\nTIME PROGRESSING")################################################################
+        #outerloop for time progress
+        n = 1
+        while n < (T+1) :
+            #update time-dependent boundary
+            t.assign(n)
+            print("t is: ",n*dt)
+            print("n is: ",n)
 
-        print("\n1)PREDICTOR")##################################################################
-        #loop for non-linearity
-        while(True):  
+            if bc_tang:
+                bc_tang[0]=[bc_tang[0][0].project(bc_expr),4]
+                bc_tang[1]=[bc_tang[1][0].project(bc_expr),1]
+                bc_tang[2]=[bc_tang[2][0].project(bc_expr),2]
+                bc_tang[3]=[bc_tang[3][0].project(bc_expr),3]
 
-            #solve predictorform
-            solver_pred.solve()
-
-            #convergence criterion
-            eps=errornorm(v_k,w_pred)#l2 by default          
-            counter+=1
-            print("Picard iteration counter: ",counter,"Picard iteration norm: ",eps)
-            if(counter>6):#eps<10**(-8)):
-                print("Picard iteration converged")  
-                break      
-            else:
-                v_k.assign(w_pred)
         
+            #update start value of picard iteration       
+            counter=0
+            v_k.assign(u_n)
 
-        print("\n2) PRESSURE UPDATE")#########################################################
-        #first modify update form
-        div_old_temp=Function(P).project(div(w_pred))
-        div_old.assign(div_old_temp)
-        print("Div error of predictor velocity",errornorm(div_old,Function(P)))
+            with PETSc.Log.Event("picard iteration"):
+                print("\n1)PREDICTOR")##################################################################
+                #loop for non-linearity
+                while(True):  
 
-        #solve update equation
-        solver_upd.solve()
-        wsol,betasol=w_upd.split()
+                    with PETSc.Log.Event("predictor solve"):
+                        solver_pred.solve()
 
-        #update pressure
-        p_knew=Function(P).assign(p_n+betasol)
+                    #convergence criterion
+                    eps=errornorm(v_k,w_pred)#l2 by default          
+                    counter+=1
+                    print("Picard iteration counter: ",counter,"Picard iteration norm: ",eps)
+                    if(counter>6):#eps<10**(-8)):
+                        print("Picard iteration converged")  
+                        break      
+                    else:
+                        v_k.assign(w_pred)
+            
+                print("\n2) PRESSURE UPDATE")#########################################################
+                #first modify update form
+                div_old_temp=Function(P).project(div(w_pred))
+                div_old.assign(div_old_temp)
+                print("Div error of predictor velocity",errornorm(div_old,Function(P)))
 
+                #solve update equation
+                with PETSc.Log.Event("update solve"):
+                    solver_upd.solve()
+                wsol,betasol=w_upd.split()
 
-        print("\n3) CORRECTOR")##############################################################
-        #first modify corrector form        
-        v_knew_hat.assign(w_pred)
-        beta.assign(betasol)
-        
-        #then solve 
-        solver_corr.solve()
-        usol=Function(U).assign(w_corr)
+                #update pressure
+                p_knew=Function(P).assign(p_n+betasol)
 
-       # plot(usol)
-       # plt.show()
+                print("\n3) CORRECTOR")##############################################################
+                #first modify corrector form        
+                v_knew_hat.assign(w_pred)
+                beta.assign(betasol)
+                
+                #then solve 
+                with PETSc.Log.Event("corrector solve"):
+                    solver_corr.solve()
+                usol=Function(U).assign(w_corr)
 
-        #divtest
-        divtest=Function(P).project(div(usol))
-        print("Div error of corrector velocity",errornorm(divtest,Function(P)))
+            #divtest
+            divtest=Function(P).project(div(usol))
+            print("Div error of corrector velocity",errornorm(divtest,Function(P)))
 
-        #update for next time step
-        u_n.assign(usol)
-        p_n.assign(p_knew)
-        #usol=Function(U)
-        #psol=Function(P)
-        #wsol=Function(U)
-        #betasol=Function(P)
+            #update for next time step
+            u_n.assign(usol)
+            p_n.assign(p_knew)
 
-        #write in vtk file
-        if output:
-            outfile.write(u_n,p_n,time=n)
-        n += 1     
+            #write in vtk file
+            if output:
+                outfile.write(u_n,p_n,time=n)
+            n += 1     
 
 
     #final time step solution
--- verification/p_convergence.py
+++ verification/p_convergence.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def run_p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output):
+def p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output):
     #various erros for velocity and pressure
     linf_error_pres_list=[]
     l2_error_pres_list=[]
@@ -45,9 +45,9 @@ def run_p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output):
         
             #solve
             if bc_type=="periodic":
-                _,err_u,err_p,_ = taylorgreen(dx,D,t_params,RE,True,output)
+                _,err_u,err_p,_,_ = taylorgreen(dx,D,t_params,RE,True,output)
             elif bc_type=="dirichlet":
-                _,err_u,err_p,_ = taylorgreen(dx,D,t_params,RE,False,output)
+                _,err_u,err_p,_,_ = taylorgreen(dx,D,t_params,RE,False,output)
 
             #update list
             linf_error_velo.append(err_u[0])
@@ -89,7 +89,7 @@ def run_p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output):
 
         #write convergence rates to csv file
         data_file=pd.DataFrame(data)
-        result="results/taylorgreen_CFL%d_RE%d_TMAX%d_XLEN%d_N%d_BC%s.csv"%(cfl,RE,TMAX,XLEN,N,bc_type)
+        result="verification/results/taylorgreen_CFL%d_RE%d_TMAX%d_XLEN%d_N%d_BC%s.csv"%(cfl,RE,TMAX,XLEN,N,bc_type)
         data_file.to_csv(result, index=False, mode="w")
 
     #plot convergence rate
@@ -115,45 +115,3 @@ def run_p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output):
     axis_pres.legend()
     plt.show()
     plt.savefig("convergence.pdf", dpi=150)
-
-
-
-
-
-#####################MAIN#############################
-#plottin spatial convergence for taylor green vortices
-
-
-cfl_list=[20,10,8,4,2,1,0.1]#cfl number
-order_list=range(1,5)#space dimension
-RE=1#reynolds number
-N=6#5#fe number (space discretisation)
-TMAX=1
-XLEN=2*pi
-bc_type="dirichlet"
-output=False
-
-###various re
-tmp0=[0.004199928234239094, 0.0006203590189461528, 0.00027357106048593594, 0.00014612394787934246]#1.5,1.5/4..
-tmp=[0.12506863431237716, 0.0015602447017334254, 0.0003541209172361233, 0.0002269698665969197]
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=1.5,N=5,T=1s")
-tmp0=[0.0018910704375055933, 0.00020123617502074143, 8.327579420473914e-05, 2.2441518410518287e-05]#n=6, cfl=2
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=2,N=6,T=1s")
-tmp0=[0.010373937727772505, 0.00168355950512189, 0.0006003826762952247, 0.0002101329615306069]#n=5,cfl=3
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=3,N=5,T=1s")
-tmp0=[0.00668929374320992, 0.00337689363074189, 0.0004213895415813296, 0.0003333310949201508]#n=5, cfl=4
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=4,N=5,T=1s")
-tmp0=[0.006913096228617827, 0.0007296063034354094, 0.00023802938457741403, 8.561598912988459e-05]
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=2,N=5,T=1s")
-plt.show()
-
-
-#right  cfl
-tmp0=[0.004575733232865732, 6.511989489434156e-05, 2.6288564754922694e-05, 1.4757693163211516e-05]
-plot_convergence_velo_pres(tmp0,tmp,[1,2,3,4],0.0001,"cfl=0.2,N=6,T=0.01s")
-plt.show()
-
-run_p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output)
-
-
-#############################################################################
