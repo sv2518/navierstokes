85e8e60f00d96d861e925531f278b109f767eb91
Sophia Vorderwuelbecke <sv2518@dyn47-217.me.ic.ac.uk>
2019-07-26 13:20:44 +0100

    profiling

--- performance/profiling.py
+++ performance/profiling.py
@@ -12,15 +12,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pyop2.profiling import timed_stage
 from firedrake.petsc import PETSc
+from mpi4py import MPI
+
 
 parameters["pyop2_options"]["lazy_evaluation"] = False
-
 
 cfl_list=[0.1]#cfl number
 order=1#space dimension
 RE=1#reynolds number
 N=6#5#fe number (space discretisation)
-TMAX=0.01
+TMAX=0.1
 XLEN=2*pi
 bc_type="dirichlet"
 output=False
@@ -34,16 +35,35 @@ t_params=[dt,T]
 #dx defined over element number & space dimensions
 dx=XLEN/2**N
 
-
+#initiate the logging
 PETSc.Log.begin()
 
-#with timed_stage("taylorgreen"):
-with PETSc.Log.Stage("taylorgreen"):
-    _,err_u,err_p,_ = taylorgreen(dx,order,t_params,RE,False,output)
+with PETSc.Log.Event("taylorgreen"):
+    _,err_u,err_p,_,comm = taylorgreen(dx,order,t_params,RE,False,output)
 
-PETSc.Log.Stage("TG").push()
+#gather all time information
+time_data={
+    "taylorgreen":PETSc.Log.Event("taylorgreen").getPerfInfo()["time"],
+    "configuration":PETSc.Log.Event("configuration").getPerfInfo()["time"],
+    "spcs":PETSc.Log.Event("spcs").getPerfInfo()["time"],
+    "spcs configuration":PETSc.Log.Event("spcs configuration").getPerfInfo()["time"],
+    "initial values":PETSc.Log.Event("initial values").getPerfInfo()["time"],
+    "build forms":PETSc.Log.Event("initial values").getPerfInfo()["time"],
+    "build problems and solvers":PETSc.Log.Event("build problems and solvers").getPerfInfo()["time"],
+    "predictor":PETSc.Log.Event("predictor").getPerfInfo()["time"],
+    "update":PETSc.Log.Event("update").getPerfInfo()["time"],
+    "corrector":PETSc.Log.Event("corrector").getPerfInfo(),
+    "time progressing":PETSc.Log.Event("time progressing").getPerfInfo()["time"],
+    "picard iteration":PETSc.Log.Event("picard iteration").getPerfInfo()["time"],
+    "predictor solve":PETSc.Log.Event("predictor solve").getPerfInfo()["time"],
+    "update solve":PETSc.Log.Event("update solve").getPerfInfo()["time"],
+    "corrector solve":PETSc.Log.Event("corrector solve").getPerfInfo()["time"],
+    "postprocessing":PETSc.Log.Event("postprocessing").getPerfInfo()["time"]
+}
 
-
-tg = PETSc.Log.Event("TG").getPerfInfo()
-
-print(tg)
+#write out data to .csv
+datafile= pd.DataFrame(time_data, index=[0])
+result="results/timedata_taylorgreen_CFL%d_RE%d_TMAX%d_XLEN%d_N%d_BC%s.csv"%(cfl_list[0],RE,TMAX,XLEN,N,bc_type)
+if not os.path.exists(os.path.dirname('results/')):
+        os.makedirs(os.path.dirname('results/'))
+datafile.to_csv(result, index=False,mode="w", header=True)
