1d8d317706086567bb15881d1a0629ee659de603
Sophia Vorderwuelbecke <sv2518@dyn47-217.me.ic.ac.uk>
2019-08-08 12:48:39 +0100

    verification

--- run_p_convergence.py
+++ run_p_convergence.py
@@ -1,14 +1,14 @@
 from verification.p_convergence import p_convergence
 from firedrake import *
 
-
-cfl_list=[20,15,10,8,4,2,1,0.1]#cfl number
+cfl_list=[0.1]
+#cfl_list=[20,15,10,8,4,2,1,0.1]#cfl number
 order_list=range(1,5)#space dimension
 RE=1#reynolds number
 N=6#5#fe number (space discretisation)
 TMAX=1
-XLEN=pi
+XLEN=2*pi
 bc_type="dirichlet"
 output=False
 
-p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output,"newstabs2")
+p_convergence(cfl_list,order_list,RE,TMAX,XLEN,N,bc_type,output,"newstabs")
