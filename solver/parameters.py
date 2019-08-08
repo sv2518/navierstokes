23d0294b25db57cb084cbd369037bc09f7b30b57
Sophia Vorderwuelbecke <sv2518@dyn47-217.me.ic.ac.uk>
2019-08-08 09:51:27 +0100

    profiling

--- solver/parameters.py
+++ solver/parameters.py
@@ -94,8 +94,8 @@ def defineSolverParameters():
      #   "mat_type":"aij"
     }
 
-    spsc_direct_params={parameters_velo_direct,parameters_pres_direct,parameters_corr_direct} 
-    spsc_iter_params={parameters_velo_iter,parameters_pres_iter,parameters_corr_iter} 
-    spsc_initial_params={params_velo_inital}
+    spsc_direct_params=[parameters_velo_direct,parameters_pres_direct,parameters_corr_direct] 
+    spsc_iter_params=[parameters_velo_iter,parameters_pres_iter,parameters_corr_iter] 
+    spsc_initial_params=[params_velo_inital]
     return [spsc_direct_params,spsc_iter_params,spsc_initial_params]
     #return [parameters_pres_iter,parameters_corr_iter,parameters_pres,parameters_pres_better,parameters_velo_iter,parameters_velo_initial,parameters_kovasznay]
